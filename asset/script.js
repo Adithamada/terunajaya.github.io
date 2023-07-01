0ef54f42641645d42673bec6406af02ee385b8e5
Adithamada <sukradithama30@gmail.com>
2023-07-01 12:40:49 +0800

    commit pertama

---
+++ asset/script.js
@@ -0,0 +1,8 @@
+// burger navbar //
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('nav');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+};
